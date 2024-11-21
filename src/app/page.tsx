d6c50cd827a6d11bd390831cad89d82ea0feb79c
Ale Rovella <alrovella@gmail.com>
2024-11-21 14:44:39 -0300

    chore: GOOGLE_GENERATIVE_AI_API_KEY check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import SearchSection from "@/components/SearchSection";
 
 export default function Home() {
-  return <SearchSection />;
+  return (
+    <>
+      <p>{process.env.GOOGLE_GENERATIVE_AI_API_KEY ? "ok" : "no"}</p>
+      <SearchSection />
+    </>
+  );
 }
